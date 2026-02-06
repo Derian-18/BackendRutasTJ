97f7efd147d4ab61a7e0a39c7065237b2ceda54c
Derian-18 <derian.jimenezramirez@cesunbc.edu.mx>
2026-02-06 09:45:44 -0800

    feat: se agrego vista para admin.html

--- core_rutasTJ/panel/urls.py
+++ core_rutasTJ/panel/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path("login/", views.login_view, name="login"),
+    path('panel_admin/', views.panel_admin, name="panel_admin")
 ]
