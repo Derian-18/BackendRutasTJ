ef0d8b978e89db9b8f41786c1143e36168257b82
Derian-18 <derian.jimenezramirez@cesunbc.edu.mx>
2026-02-08 16:14:04 -0800

    Revert "Merge pull request #2 from Derian-18/derian"
    This reverts commit 5dd8bb795932bec62fe7fcc08239ea96444d283f, reversing
    changes made to 4ff299ca3f0ecf1416698f686c62075ca21b1dfa.

--- core_rutasTJ/panel/views.py
+++ core_rutasTJ/panel/views.py
@@ -7,10 +7,6 @@ from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     return render(request, 'index.html')
-
-def panel_admin(request):
-    return render(request, 'admin.html')
-
 # Iniciar sesion
 def login_view(request):
     if request.method == "POST":
@@ -21,7 +17,7 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            return redirect("panel_admin")  # Cambia a tu vista principal
+            return redirect("index.html")  # Cambia a tu vista principal
         else:
             messages.error(request, "Usuario o contraseña incorrectos")
 
